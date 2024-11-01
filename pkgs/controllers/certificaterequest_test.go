nassah/origin-ca-issuer

commit d8c1b77425c2755f7907de14f41a10f6a0add74e

feat: add ClusterOriginIssuer
When using this controller in single-tenant clusters it becomes
desirable for many users to have a single cluster-scoped issuer rather
than each namespace duplicating the issuers and secrets.

This changeset introduces a "cluster resource namespace", which defaults
to the same namespace as the controller (eg, "origin-ca-issuer"). A
ClusterOriginIssuer resource type has been added which resolves
SecretKeySelectors in this namespace. Other than determing what
namespace to resolve the secret, the implementation is the same as the
existing OriginIssuer.

Fixes: #24

pkgs/controllers/certificaterequest.go
@@ -27,9 +27,10 @@ const originDBWriteErrorCode = 1100
 // that references this controller.
 type CertificateRequestController struct {
 	client.Client
-	Reader  client.Reader
-	Log     logr.Logger
-	Factory cfapi.Factory
+	Reader                   client.Reader
+	ClusterResourceNamespace string
+	Log                      logr.Logger
+	Factory                  cfapi.Factory
 
 	Clock                  clock.Clock
 	CheckApprovedCondition bool
@@ -110,32 +111,74 @@ func (r *CertificateRequestController) Reconcile(ctx context.Context, cr *certma
 		return reconcile.Result{}, nil
 	}
 
-	iss := v1.OriginIssuer{}
-	issNamespaceName := types.NamespacedName{
-		Namespace: cr.Namespace,
-		Name:      cr.Spec.IssuerRef.Name,
-	}
+	var (
+		secretNamespaceName types.NamespacedName
+		issuerspec          v1.OriginIssuerSpec
+	)
 
-	if err := r.Client.Get(ctx, issNamespaceName, &iss); err != nil {
-		log.Error(err, "failed to retrieve OriginIssuer resource", "namespace", issNamespaceName.Namespace, "name", issNamespaceName.Name)
-		_ = r.setStatus(ctx, cr, cmmeta.ConditionFalse, certmanager.CertificateRequestReasonPending, fmt.Sprintf("Failed to retrieve OriginIssuer resource %s: %v", issNamespaceName, err))
+	switch cr.Spec.IssuerRef.Kind {
+	case "OriginIssuer":
+		iss := v1.OriginIssuer{}
+		issNamespaceName := types.NamespacedName{
+			Namespace: cr.Namespace,
+			Name:      cr.Spec.IssuerRef.Name,
+		}
+
+		if err := r.Client.Get(ctx, issNamespaceName, &iss); err != nil {
+			log.Error(err, "failed to retrieve OriginIssuer resource", "namespace", issNamespaceName.Namespace, "name", issNamespaceName.Name)
+			_ = r.setStatus(ctx, cr, cmmeta.ConditionFalse, certmanager.CertificateRequestReasonPending, fmt.Sprintf("Failed to retrieve OriginIssuer resource %s: %v", issNamespaceName, err))
+
+			return reconcile.Result{}, err
+		}
+
+		if !IssuerStatusHasCondition(iss.Status, v1.OriginIssuerCondition{Type: v1.ConditionReady, Status: v1.ConditionTrue}) {
+			err := fmt.Errorf("resource %s is not ready", issNamespaceName)
+			log.Error(err, "issuer failed readiness checks", "namespace", issNamespaceName.Namespace, "name", issNamespaceName.Name)
+			_ = r.setStatus(ctx, cr, cmmeta.ConditionFalse, certmanager.CertificateRequestReasonPending, fmt.Sprintf("OriginIssuer %s is not Ready", issNamespaceName))
+
+			return reconcile.Result{}, err
+		}
+
+		secretNamespaceName = types.NamespacedName{
+			Namespace: iss.Namespace,
+			Name:      iss.Spec.Auth.ServiceKeyRef.Name,
+		}
+		issuerspec = iss.Spec
+	case "ClusterOriginIssuer":
+		iss := v1.ClusterOriginIssuer{}
+		issNamespaceName := types.NamespacedName{
+			Name: cr.Spec.IssuerRef.Name,
+		}
+
+		if err := r.Client.Get(ctx, issNamespaceName, &iss); err != nil {
+			log.Error(err, "failed to retrieve OriginIssuer resource", "namespace", issNamespaceName.Namespace, "name", issNamespaceName.Name)
+			_ = r.setStatus(ctx, cr, cmmeta.ConditionFalse, certmanager.CertificateRequestReasonPending, fmt.Sprintf("Failed to retrieve OriginIssuer resource %s: %v", issNamespaceName, err))
+
+			return reconcile.Result{}, err
+		}
+
+		if !IssuerStatusHasCondition(iss.Status, v1.OriginIssuerCondition{Type: v1.ConditionReady, Status: v1.ConditionTrue}) {
+			err := fmt.Errorf("resource %s is not ready", issNamespaceName)
+			log.Error(err, "issuer failed readiness checks", "namespace", issNamespaceName.Namespace, "name", issNamespaceName.Name)
+			_ = r.setStatus(ctx, cr, cmmeta.ConditionFalse, certmanager.CertificateRequestReasonPending, fmt.Sprintf("OriginIssuer %s is not Ready", issNamespaceName))
+
+			return reconcile.Result{}, err
+		}
+
+		secretNamespaceName = types.NamespacedName{
+			Namespace: r.ClusterResourceNamespace,
+			Name:      iss.Spec.Auth.ServiceKeyRef.Name,
+		}
+		issuerspec = iss.Spec
+	default:
+		err := fmt.Errorf("unknown issuer kind: %s", cr.Spec.IssuerRef.Kind)
+		log.Error(err, "certificate request references unknown issuer kind", "namespace", cr.Namespace, "name", cr.Name)
+		_ = r.setStatus(ctx, cr, cmmeta.ConditionFalse, certmanager.CertificateRequestReasonFailed, fmt.Sprintf("Unknown issuer kind: %s", cr.Spec.IssuerRef.Kind))
 
 		return reconcile.Result{}, err
 	}
 
-	if !IssuerHasCondition(iss, v1.OriginIssuerCondition{Type: v1.ConditionReady, Status: v1.ConditionTrue}) {
-		err := fmt.Errorf("resource %s is not ready", issNamespaceName)
-		log.Error(err, "issuer failed readiness checks", "namespace", issNamespaceName.Namespace, "name", issNamespaceName.Name)
-		_ = r.setStatus(ctx, cr, cmmeta.ConditionFalse, certmanager.CertificateRequestReasonPending, fmt.Sprintf("OriginIssuer %s is not Ready", issNamespaceName))
-
-		return reconcile.Result{}, err
-	}
-
-	secret := core.Secret{}
-	secretNamespaceName := types.NamespacedName{
-		Namespace: iss.Namespace,
-		Name:      iss.Spec.Auth.ServiceKeyRef.Name,
-	}
+	var secret core.Secret
 	if err := r.Reader.Get(ctx, secretNamespaceName, &secret); err != nil {
 		log.Error(err, "failed to retieve OriginIssuer auth secret", "namespace", secretNamespaceName.Namespace, "name", secretNamespaceName.Name)
 		if apierrors.IsNotFound(err) {
@@ -147,9 +190,9 @@ func (r *CertificateRequestController) Reconcile(ctx context.Context, cr *certma
 		return reconcile.Result{}, err
 	}
 
-	serviceKey, ok := secret.Data[iss.Spec.Auth.ServiceKeyRef.Key]
+	serviceKey, ok := secret.Data[issuerspec.Auth.ServiceKeyRef.Key]
 	if !ok {
-		err := fmt.Errorf("secret %s does not contain key %q", secret.Name, iss.Spec.Auth.ServiceKeyRef.Key)
+		err := fmt.Errorf("secret %s does not contain key %q", secret.Name, issuerspec.Auth.ServiceKeyRef.Key)
 		log.Error(err, "failed to retrieve OriginIssuer auth secret")
 		_ = r.setStatus(ctx, cr, cmmeta.ConditionFalse, "NotFound", fmt.Sprintf("Failed to retrieve auth secret: %v", err))
 
@@ -163,7 +206,7 @@ func (r *CertificateRequestController) Reconcile(ctx context.Context, cr *certma
 		return reconcile.Result{}, err
 	}
 
-	p, err := provisioners.New(c, iss.Spec.RequestType, log)
+	p, err := provisioners.New(c, issuerspec.RequestType, log)
 	if err != nil {
 		log.Error(err, "failed to create provisioner")
 

pkgs/controllers/certificaterequest_test.go
@@ -47,7 +47,7 @@ func TestCertificateRequestReconcile(t *testing.T) {
 		namespaceName types.NamespacedName
 	}{
 		{
-			name: "working",
+			name: "working OriginIssuer",
 			objects: []runtime.Object{
 				cmgen.CertificateRequest("foobar",
 					cmgen.SetCertificateRequestNamespace("default"),
@@ -92,6 +92,85 @@ func TestCertificateRequestReconcile(t *testing.T) {
 					ObjectMeta: metav1.ObjectMeta{
 						Name:      "service-key-issuer",
 						Namespace: "default",
+					},
+					Data: map[string][]byte{
+						"key": []byte("djEuMC0weDAwQkFCMTBD"),
+					},
+				},
+			},
+			signer: SignerFunc(func(ctx context.Context, sr *cfapi.SignRequest) (*cfapi.SignResponse, error) {
+				return &cfapi.SignResponse{
+					Id:          "1",
+					Certificate: "bogus",
+					Hostnames:   []string{"example.com"},
+					Expiration:  time.Time{},
+					Type:        "colemak",
+					Validity:    0,
+					CSR:         "foobar",
+				}, nil
+			}),
+			expected: cmapi.CertificateRequestStatus{
+				Conditions: []cmapi.CertificateRequestCondition{
+					{
+						Type:               cmapi.CertificateRequestConditionReady,
+						Status:             cmmeta.ConditionTrue,
+						LastTransitionTime: &now,
+						Reason:             "Issued",
+						Message:            "Certificate issued",
+					},
+				},
+				Certificate: []byte("bogus"),
+			},
+			namespaceName: types.NamespacedName{
+				Namespace: "default",
+				Name:      "foobar",
+			},
+		},
+		{
+			name: "working ClusterOriginIssuer",
+			objects: []runtime.Object{
+				cmgen.CertificateRequest("foobar",
+					cmgen.SetCertificateRequestNamespace("default"),
+					cmgen.SetCertificateRequestDuration(&metav1.Duration{Duration: 7 * 24 * time.Hour}),
+					cmgen.SetCertificateRequestCSR((func() []byte {
+						csr, _, err := cmgen.CSR(x509.ECDSA)
+						if err != nil {
+							t.Fatalf("creating CSR: %s", err)
+						}
+
+						return csr
+					})()),
+					cmgen.SetCertificateRequestIssuer(cmmeta.ObjectReference{
+						Name:  "foobar",
+						Kind:  "ClusterOriginIssuer",
+						Group: "cert-manager.k8s.cloudflare.com",
+					}),
+				),
+				&v1.ClusterOriginIssuer{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "foobar",
+					},
+					Spec: v1.OriginIssuerSpec{
+						Auth: v1.OriginIssuerAuthentication{
+							ServiceKeyRef: v1.SecretKeySelector{
+								Name: "service-key-issuer",
+								Key:  "key",
+							},
+						},
+					},
+					Status: v1.OriginIssuerStatus{
+						Conditions: []v1.OriginIssuerCondition{
+							{
+								Type:   v1.ConditionReady,
+								Status: v1.ConditionTrue,
+							},
+						},
+					},
+				},
+				&corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "service-key-issuer",
+						Namespace: "super-secret",
 					},
 					Data: map[string][]byte{
 						"key": []byte("djEuMC0weDAwQkFCMTBD"),
@@ -203,9 +282,10 @@ func TestCertificateRequestReconcile(t *testing.T) {
 				Build()
 
 			controller := &CertificateRequestController{
-				Client: client,
-				Reader: client,
-				Log:    logf.Log,
+				Client:                   client,
+				Reader:                   client,
+				ClusterResourceNamespace: "super-secret",
+				Log:                      logf.Log,
 				Factory: cfapi.FactoryFunc(func(serviceKey []byte) (cfapi.Interface, error) {
 					return tt.signer, nil
 				}),

cmd/controller/main.go
@@ -100,11 +100,30 @@ func main() {
 
 	err = builder.
 		ControllerManagedBy(mgr).
+		For(&v1.ClusterOriginIssuer{}).
+		Complete(reconcile.AsReconciler(mgr.GetClient(), &controllers.ClusterOriginIssuerController{
+			Client:                   mgr.GetClient(),
+			Reader:                   mgr.GetAPIReader(),
+			ClusterResourceNamespace: o.ClusterResourceNamespace,
+			Clock:                    clock.RealClock{},
+			Factory:                  f,
+			Log:                      log.WithName("controllers").WithName("ClusterOriginIssuer"),
+		}))
+
+	if err != nil {
+		log.Error(err, "could not create cluster origin issuer controller")
+		os.Exit(1)
+	}
+
+	err = builder.
+		ControllerManagedBy(mgr).
 		For(&certmanager.CertificateRequest{}).
 		Complete(reconcile.AsReconciler(mgr.GetClient(), &controllers.CertificateRequestController{
-			Client: mgr.GetClient(),
-			Reader: mgr.GetAPIReader(),
-			Log:    log.WithName("controllers").WithName("CertificateRequest"),
+			Client:                   mgr.GetClient(),
+			Reader:                   mgr.GetAPIReader(),
+			ClusterResourceNamespace: o.ClusterResourceNamespace,
+			Factory:                  f,
+			Log:                      log.WithName("controllers").WithName("CertificateRequest"),
 
 			Clock:                  clock.RealClock{},
 			CheckApprovedCondition: !o.DisableApprovedCheck,

cmd/controller/options/options.go
@@ -7,8 +7,9 @@ import (
 )
 
 type ControllerOptions struct {
-	KubernetesAPIQPS   float32
-	KubernetesAPIBurst int
+	KubernetesAPIQPS         float32
+	KubernetesAPIBurst       int
+	ClusterResourceNamespace string
 
 	DisableApprovedCheck bool
 }
@@ -29,6 +30,7 @@ func (o *ControllerOptions) AddFlags(fs *pflag.FlagSet) {
 	fs.Float32Var(&o.KubernetesAPIQPS, "kube-api-qps", defaultKubernetesAPIQPS, "Maximium queries-per-second of requests to the Kubernetes apiserver.")
 	fs.IntVar(&o.KubernetesAPIBurst, "kube-api-burst", defaultKubernetesAPIBurst, "Maximium queries-per-second burst of request send to the Kubernetes apiserver.")
 	fs.BoolVar(&o.DisableApprovedCheck, "disable-approved-check", o.DisableApprovedCheck, "Disables waiting for CertificateRequests to have an approved condition before signing.")
+	fs.StringVar(&o.ClusterResourceNamespace, "cluster-resource-namespace", o.ClusterResourceNamespace, "Namespace used for cluster-scoped resources, such as secrets used by ClusterOriginIssuer")
 }
 
 func (o *ControllerOptions) Validate() error {
@@ -38,6 +40,10 @@ func (o *ControllerOptions) Validate() error {
 
 	if o.KubernetesAPIQPS <= 0 {
 		return fmt.Errorf("invalid value for kube-api-qps: %v must be higher than 0", o.KubernetesAPIQPS)
+	}
+
+	if o.ClusterResourceNamespace == "" {
+		return fmt.Errorf("invalid value for cluster-resource-namespace: must be set")
 	}
 
 	return nil

deploy/crds/cert-manager.k8s.cloudflare.com_clusteroriginissuers.yaml
@@ -4,22 +4,22 @@ kind: CustomResourceDefinition
 metadata:
   annotations:
     controller-gen.kubebuilder.io/version: v0.13.0
-  name: originissuers.cert-manager.k8s.cloudflare.com
+  name: clusteroriginissuers.cert-manager.k8s.cloudflare.com
 spec:
   group: cert-manager.k8s.cloudflare.com
   names:
-    kind: OriginIssuer
-    listKind: OriginIssuerList
-    plural: originissuers
-    singular: originissuer
-  scope: Namespaced
+    kind: ClusterOriginIssuer
+    listKind: ClusterOriginIssuerList
+    plural: clusteroriginissuers
+    singular: clusteroriginissuer
+  scope: Cluster
   versions:
   - name: v1
     schema:
       openAPIV3Schema:
-        description: An OriginIssuer represents the Cloudflare Origin CA as an external
-          cert-manager issuer. It is scoped to a single namespace, so it can be used
-          only by resources in the same namespace.
+        description: A ClusterOriginIssuer represents the Cloudflare Origin CA as
+          an external cert-manager issuer. It is scoped to a single namespace, so
+          it can be used only by resources in the same namespace.
         properties:
           apiVersion:
             description: 'APIVersion defines the versioned schema of this representation
@@ -34,7 +34,7 @@ spec:
           metadata:
             type: object
           spec:
-            description: Desired state of the OriginIssuer resource
+            description: Spec is the desired state of the ClusterOriginIssuer resource.
             properties:
               auth:
                 description: Auth configures how to authenticate with the Cloudflare
@@ -70,7 +70,8 @@ spec:
             - requestType
             type: object
           status:
-            description: Status of the OriginIssuer. This is set and managed automatically.
+            description: Status of the ClusterOriginIssuer. This is set and managed
+              automatically.
             properties:
               conditions:
                 description: List of status conditions to indicate the status of an

deploy/manifests/deployment.yaml
@@ -15,8 +15,15 @@ spec:
     spec:
       serviceAccountName: originissuer-control
       containers:
-        - image: cloudflare/origin-ca-issuer:v0.8.0
-          name: origin-ca-controller
+        - name: origin-ca-controller
+          image: cloudflare/origin-ca-issuer:v0.8.0
+          args:
+            - --cluster-resource-namespace=${POD_NAMESPACE}
+          env:
+            - name: POD_NAMESPACE
+              valueFrom:
+                fieldRef:
+                  fieldPath: metadata.namespace
           resources:
             limits:
               cpu: "1"

deploy/rbac/role-approver.yaml
@@ -1,4 +1,3 @@
-
 ---
 # permissions to approve all cert-manager.k8s.cloudflare.com requests
 apiVersion: rbac.authorization.k8s.io/v1
@@ -6,11 +5,12 @@ kind: ClusterRole
 metadata:
   name: cert-manager-controller-approve:cert-manager-k8s-cloudflare-com
 rules:
-- apiGroups:
-  - cert-manager.io
-  resources:
-  - signers
-  verbs:
-  - approve
-  resourceNames:
-  - originissuers.cert-manager.k8s.cloudflare.com/*
+  - apiGroups:
+      - cert-manager.io
+    resources:
+      - signers
+    verbs:
+      - approve
+    resourceNames:
+      - originissuers.cert-manager.k8s.cloudflare.com/*
+      - clusteroriginissuers.cert-manager.k8s.cloudflare.com/*

deploy/rbac/role.yaml
@@ -39,6 +39,23 @@ rules:
 - apiGroups:
   - cert-manager.k8s.cloudflare.com
   resources:
+  - clusteroriginissuers
+  verbs:
+  - create
+  - get
+  - list
+  - watch
+- apiGroups:
+  - cert-manager.k8s.cloudflare.com
+  resources:
+  - clusteroriginissuers/status
+  verbs:
+  - get
+  - patch
+  - update
+- apiGroups:
+  - cert-manager.k8s.cloudflare.com
+  resources:
   - originissuers
   verbs:
   - create

commit 2ed2a720afe1812e809c25484f806d47e865ae42

fix(certificaterequest): ignore empty issuer group
A CertificateRequest's reference to an Issuer can have an empty Group,
which we were considering as equivalent to matching, then later
rejecting the CertificateRequest when it was for an unknown issuer.
While setting the group is optional in the schema, our documentation has
always included the group in the issuer reference. The rejection was
racing with other controllers, including the built-in controllers, and
preventing them from issuing certificates.

This changeset updates CertificateRequestController to ignore
CertificateRequests where the issuer group is unset, preventing the
race with controllers for other issuers. We'll continue to reject
CertificateRequests for unknown kinds if the group matches our group.

Bug: #149
Fixes: d8c1b77 ("feat: add ClusterOriginIssuer")

pkgs/controllers/certificaterequest.go
@@ -44,7 +44,7 @@ type CertificateRequestController struct {
 func (r *CertificateRequestController) Reconcile(ctx context.Context, cr *certmanager.CertificateRequest) (reconcile.Result, error) {
 	log := r.Log.WithValues("namespace", cr.Namespace, "certificaterequest", cr.Name)
 
-	if cr.Spec.IssuerRef.Group != "" && cr.Spec.IssuerRef.Group != v1.GroupVersion.Group {
+	if cr.Spec.IssuerRef.Group != v1.GroupVersion.Group {
 		log.V(4).Info("resource does not specify an issuerRef group name that we are responsible for", "group", cr.Spec.IssuerRef.Group)
 
 		return reconcile.Result{}, nil

pkgs/controllers/certificaterequest_test.go
@@ -303,6 +303,57 @@ func TestCertificateRequestReconcile(t *testing.T) {
 			},
 		},
 		{
+			name: "ignores CertificateRequests with empty Issuer group reference",
+			objects: []runtime.Object{
+				cmgen.CertificateRequest("foobar",
+					cmgen.SetCertificateRequestNamespace("default"),
+					cmgen.SetCertificateRequestDuration(&metav1.Duration{Duration: 7 * 24 * time.Hour}),
+					cmgen.SetCertificateRequestCSR(golden.Get(t, "csr.golden")),
+					cmgen.SetCertificateRequestIssuer(cmmeta.ObjectReference{
+						Name: "foobar",
+						Kind: "StepIssuer", // 👋 hello friends!
+					}),
+				),
+				&v1.ClusterOriginIssuer{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "foobar",
+					},
+					Spec: v1.OriginIssuerSpec{
+						RequestType: v1.RequestTypeOriginECC,
+						Auth: v1.OriginIssuerAuthentication{
+							TokenRef: &v1.SecretKeySelector{
+								Name: "token-issuer",
+								Key:  "token",
+							},
+						},
+					},
+					Status: v1.OriginIssuerStatus{
+						Conditions: []v1.OriginIssuerCondition{
+							{
+								Type:   v1.ConditionReady,
+								Status: v1.ConditionTrue,
+							},
+						},
+					},
+				},
+				&corev1.Secret{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "token-issuer",
+						Namespace: "super-secret",
+					},
+					Data: map[string][]byte{
+						"token": []byte("api-token"),
+					},
+				},
+			},
+			recorder: RecorderMust(t, "testdata/working"),
+			expected: cmapi.CertificateRequestStatus{},
+			namespaceName: types.NamespacedName{
+				Namespace: "default",
+				Name:      "foobar",
+			},
+		},
+		{
 			name: "OriginIssuer without authentication",
 			objects: []runtime.Object{
 				cmgen.CertificateRequest("foobar",